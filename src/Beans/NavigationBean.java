d938eb4def86d3ccfe6c2076a5e15637a00cb2b9
Dominic <dominnic.seitz@gmx.de>
2016-12-06 21:20:26 +0100

    NavigationBean edited

--- src/Beans/NavigationBean.java
+++ src/Beans/NavigationBean.java
@@ -7,4 +7,20 @@ import javax.faces.bean.*;
 @RequestScoped
 public class NavigationBean implements Serializable {
 
+	private static final long serialVersionUID = -5459300954012448102L;
+
+	// managed property liest den Parameter param von request aus
+	@ManagedProperty(value = "#{param.pageId}")
+	private String pageId;
+
+	public String showPage() {
+		if (pageId == null) {
+			return "index";
+		}
+		if (pageId.equals("1")) {
+			return "login";
+		} else {
+			return "home";
+		}
+	}
 }
